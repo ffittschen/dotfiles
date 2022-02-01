5102f2c8cdfbe9d1fad34fc58a71b7026fc9d84a
Florian Fittschen <ffittschen@gmail.com>
2022-02-01 12:19:25 +0100

    Increase fastlane timeout to avoid retries for projects using SPM

--- private_dot_config/fish/environment_variables.fish
+++ private_dot_config/fish/environment_variables.fish
@@ -9,3 +9,7 @@ set -gx LANG en_US.UTF-8
 
 # Skip fastlane update check
 set -gx FASTLANE_SKIP_UPDATE_CHECK 1
+
+# Increase fastlane timeout to avoid retries for projects using SPM
+set -gx FASTLANE_XCODEBUILD_SETTINGS_TIMEOUT 30
+
